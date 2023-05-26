c390f0b6a6883e01c1a4450d82ab4392e6bc89e5
NazarRom <nazarrom321@gmail.com>
2023-05-26 09:48:49 +0200

    Add project files.

---
+++ MvcApiPersonajesAWS/Controllers/PersonajesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiPersonajesAWS.Models;
+using MvcApiPersonajesAWS.Services;
+
+namespace MvcApiPersonajesAWS.Controllers
+{
+    public class PersonajesController : Controller
+    {
+        private ServiceApiPersonajes service;
+
+        public PersonajesController(ServiceApiPersonajes service)
+        {
+            this.service = service;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public async Task<IActionResult> ApiPersonajes()
+        {
+            List<Personaje> personajes = await this.service.GetPersonajesAsync();
+            return View(personajes);
+        }
+    }
+}
